36a8c03218b857a7e9ff123c5033f98d9b47a2d9
Ankur <ankurmandal.official@gmail.com>
2023-11-05 10:39:39 +0530

    implement Reponse Entity

--- src/main/java/com/example/studentmanagementapp/StudentController.java
+++ src/main/java/com/example/studentmanagementapp/StudentController.java
@@ -1,34 +1,52 @@
 package com.example.studentmanagementapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
 
     @Autowired
     StudentService studentService;
-    @GetMapping("/get")
-    public Student getStudent(@RequestParam("q") int regNo){
-        return studentService.getStudent(regNo);
-    }
+
     @PostMapping("/add")
     public String addStudent(@RequestBody Student student){
         return studentService.addStudent(student);
 
 
     }
+
+    @GetMapping("/get")
+    public ResponseEntity getStudent(@RequestParam("q") int regNo){
+        Student student=studentService.getStudent(regNo);
+        if(student==null){
+            return new ResponseEntity<>("Id doesn't exist",HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(student,HttpStatus.FOUND);
+    }
     @GetMapping("/getByPath/{id}")
-    public Student getStudentByPath(@PathVariable("id") int regNo){
-        return studentService.getStudentByPath(regNo);
+    public ResponseEntity getStudentByPath(@PathVariable("id") int regNo){
+        Student student=studentService.getStudentByPath(regNo);
+        if(student==null){
+            return new ResponseEntity<>("Id doesn't exist", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(student,HttpStatus.FOUND);
 
     }
     @PutMapping("/update-age/{id}")
-    public Student updateAge(@PathVariable ("id") int regNo,@RequestParam("age") int newAge){
-        return studentService.updateAge(regNo,newAge);
+    public ResponseEntity updateAge(@PathVariable ("id") int regNo,@RequestParam("age") int newAge){
+        Student student= studentService.updateAge(regNo,newAge);
+        if(student==null){
+            return new ResponseEntity<>("Id doesn't exist", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(student,HttpStatus.FOUND);
 
     }
 
@@ -48,20 +66,32 @@ public class StudentController {
 
     //change of a student (regNo,newCourse) --> request param
     @PutMapping("/update-course")
-    public Student updateCourse(@RequestParam("id") int regNo,@RequestParam("newCourse") String newCourse){
-        return studentService.updateCourse(regNo,newCourse);
+    public ResponseEntity updateCourse(@RequestParam("id") int regNo,@RequestParam("newCourse") String newCourse){
+        Student student= studentService.updateCourse(regNo,newCourse);
+        if(student==null){
+            return new ResponseEntity<>("Id doesn't exist", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(student,HttpStatus.FOUND);
     }
 
     //change of a student --> both as pathvarible
     @PutMapping("/updateCourseByPath/{id}/{newCourse}")
-    public Student updateCourseByPath(@PathVariable("id") int regNo,@PathVariable("newCourse") String newCourse){
-        return studentService.updateCourseByPath(regNo,newCourse);
+    public ResponseEntity updateCourseByPath(@PathVariable("id") int regNo,@PathVariable("newCourse") String newCourse){
+        Student student= studentService.updateCourseByPath(regNo,newCourse);
+        if(student==null){
+            return new ResponseEntity<>("Id doesn't exist", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(student,HttpStatus.FOUND);
 
     }
     //change course --> one as request param,one as path variable
     @PutMapping("/updateCourseByPathAndParam/{id}")
-    public Student updateCourseByPathAndParam(@PathVariable("id") int regNo,@RequestParam("newCourse") String newCourse){
-        return studentService.updateCourseByPathAndParam(regNo,newCourse);
+    public ResponseEntity updateCourseByPathAndParam(@PathVariable("id") int regNo,@RequestParam("newCourse") String newCourse){
+        Student student= studentService.updateCourseByPathAndParam(regNo,newCourse);
+        if(student==null){
+            return new ResponseEntity<>("Id doesn't exist", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(student,HttpStatus.FOUND);
 
     }
 
